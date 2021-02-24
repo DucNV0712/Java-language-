1f858d15126cff1a90546b369e88defc46f4804e
Duc NguyenVan <61277293+DucNV0712@users.noreply.github.com>
2021-02-24 22:28:29 +0700

    Assignment1

---
+++ src/Assignment1/Assignment1.java
@@ -0,0 +1,38 @@
+package Assignment1;
+import java.util.Scanner;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Nhập a:");
+        a = nhap.nextInt();
+        System.out.println("Nhập b:");
+        b = nhap.nextInt();
+        System.out.println("Nhập c:");
+        c = nhap.nextInt();
+        if(a <= 0 || b <=0 || c <= 0){
+            System.out.println("Hãy nhập số dương");
+            //kiểm tra tam giác
+
+        }else if(a + b > c && a + c  > b && c + b > a) {
+            System.out.println("Đây là một tam giác");
+            float chuvi;
+            float p;
+            //tính chu vi
+            p=(float)(a+b+c)/2;
+            chuvi = p*2;
+            System.out.println("Chu vi tam giác là: "+ chuvi);
+            //tính diện tích
+            float Dientich;
+            Dientich =(float) Math.sqrt(p*(p-a)*(p-b)*(p-c));
+            System.out.println("Diện tích tam giác là "+Dientich);
+        }else{
+            System.out.println("Đây không phải tam giác");
+        }
+
+
+    }
+}
