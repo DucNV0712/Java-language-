d0fa245689106e8bffcfee17b2b3ca6dce8ce485
Duc NguyenVan <61277293+DucNV0712@users.noreply.github.com>
2021-03-11 23:20:28 +0700

    lapS2_2

---
+++ src/Lap/S2_2/Main.java
@@ -0,0 +1,27 @@
+package Lap.S2_2;
+public class Main {
+    public static void main(String[] args) {
+        PhanSo Ps1 = new PhanSo();
+        PhanSo Ps2 = new PhanSo();
+
+
+
+
+        System.out.println("Nhập Phân Số Thứ Nhất");
+        Ps1.nhapPhanSo();
+        System.out.println("Nhập Phân Số Thư Hai");
+        Ps2.nhapPhanSo();
+
+        System.out.println("Phân Thứ Nhất Số Là");
+        Ps1.inPhanso();
+        System.out.println("Phân Thứ Hai Số Là");
+        Ps2.inPhanso();
+        System.out.println("Cộng Phân Số");
+
+        System.out.println("Trừ Phân Số");
+        System.out.println("Nhân Phân Số");
+        System.out.println("Chia Phân Số");
+
+
+    }
+}
--- src/Lap/S2_2/PhanSo.java
+++ src/Lap/S2_2/PhanSo.java
@@ -1,4 +1,4 @@
-package S2_2;
+package Lap.S2_2;
 
 import java.util.Scanner;
 
