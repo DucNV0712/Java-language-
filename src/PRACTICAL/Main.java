82b9559c7e77e566e20a1f6a7d68c4465a24a4ac
Duc NguyenVan <61277293+DucNV0712@users.noreply.github.com>
2021-03-18 11:25:04 +0700

    Bai Thi

--- src/PRACTICAL/Main.java
+++ src/PRACTICAL/Main.java
@@ -4,16 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Car c = new Car();
         Scanner nhap = new Scanner(System.in);
-        System.out.println("yeu cau nguoi dung nhap ten vao:");
+        System.out.println("Nhập Tên:");
         c.name = nhap.nextLine();
         System.out.println("ten: "+c.getName());
-        System.out.println("van toc toi da:"+c.moveMax());
+        System.out.println("Vận tốc tối đa:"+c.moveMax());
 
         Motor m = new Motor();
-        System.out.println("yeu cau nguoi dung nhap vao:");
+        System.out.println("nhập tên:");
         m.name = nhap.nextLine();
         System.out.println("ten:"+m.getName());
-        System.out.println("van toc toi da:"+m.moveMax());
+        System.out.println("vận tốc tối đa:"+m.moveMax());
 
     }
 }
